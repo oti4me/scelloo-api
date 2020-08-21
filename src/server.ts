b5f1173e35b1024d14fc15bc02111ce9861b3e3a
Henry Otighe <oti4me@Henry-Otighe-MacBook-Pro.local>
2020-08-21 02:20:16 +0100

    configure database

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,14 @@
-import express, { Application, Request, Response } from 'express';
+import express, { Application } from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
 import { config } from 'dotenv';
 import { errorHandler, notFound } from './middleware/error';
 import api from './routes/api';
+import { sync } from './database';
 
 config();
 
-const app = express();
+const app: Application = express();
 const port: number = parseInt(process.env.PORT, 10) || 3001;
 
 app.disable('x-powered-by');
@@ -20,6 +21,13 @@ app.use('/api/v1', api);
 app.use(notFound);
 app.use(errorHandler);
 
-app.listen(port, () => {
-  console.log(`Server started and listening on port ${port}`);
-});
+sync()
+  .then(() => {
+    console.log('Connected to database');
+    app.listen(port, () => {
+      console.log(`Server started and listening on port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log(`Error connecting to the database: ${error}`);
+  });
